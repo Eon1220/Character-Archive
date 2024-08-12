e1291631c4f4cde3c3cc3dcb55bda4ece4455ef0
Eon1220 <Nicolaspace21@gmail.com>
2024-08-12 04:35:01 -0400

    progress ?

--- models/Chara.js
+++ models/Chara.js
@@ -19,11 +19,6 @@ Chara.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    date_created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
